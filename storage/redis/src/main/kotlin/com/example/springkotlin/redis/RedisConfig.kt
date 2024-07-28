3ace2550dd53f712ee07e1d88a6581e340537ac4
taisooqq <cookehwk@gmail.com>
2024-07-28 15:01:37 +0900

    redis template config done

--- storage/redis/src/main/kotlin/com/example/springkotlin/redis/RedisConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.example.springkotlin.redis
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
-
-@Configuration
-@EnableRedisRepositories(basePackages = ["com.example.springkotlin.redis"])
-class RedisConfig {
-}
---
+++ storage/redis/src/main/kotlin/com/example/springkotlin/redis/config/RedisConfig.kt
@@ -0,0 +1,34 @@
+package com.example.springkotlin.redis.config
+
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+@EnableRedisRepositories(basePackages = ["com.example.springkotlin.redis"])
+class RedisConfig {
+    @Value("\${spring.redis.host}")
+    private lateinit var redisHost: String
+    @Value("\${spring.redis.port}")
+    private var redisPort: Int = 0
+
+    @Bean(name = ["redisConnectionFactory"])
+    fun redisConnectionFactory(): RedisConnectionFactory{
+        return LettuceConnectionFactory(redisHost, redisPort)
+    }
+
+    @Bean(name = ["redisTemplate"])
+    fun redisTemplate(): RedisTemplate<String, String> {
+        val redisTemplate = RedisTemplate<String, String>()
+        redisTemplate.setConnectionFactory(redisConnectionFactory())
+        redisTemplate.keySerializer = StringRedisSerializer()
+        redisTemplate.valueSerializer = StringRedisSerializer()
+        return redisTemplate
+    }
+}
