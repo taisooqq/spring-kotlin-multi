8cb54d51238168a7a55a32bf4a251205489af98a
taisooqq <cookehwk@gmail.com>
2024-08-03 17:15:06 +0900

    add restClient

--- support/rest-client/build.gradle.kts
+++ support/rest-client/build.gradle.kts
@@ -1,3 +1,4 @@
 dependencies {
     implementation("org.springframework:spring-web:6.1.0")
+    implementation("org.springframework:spring-context")
 }
--- support/rest-client/src/main/kotlin/com/example/springkotlin/restclient/RestClientConfig.kt
+++ support/rest-client/src/main/kotlin/com/example/springkotlin/restclient/RestClientConfig.kt
@@ -1,5 +1,15 @@
 package com.example.springkotlin.restclient
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestClient
 
+
+@Configuration
 class RestClientConfig {
+
+    @Bean
+    fun restClient(): RestClient {
+        return RestClient.create("https://api.naver.com")
+    }
 }
