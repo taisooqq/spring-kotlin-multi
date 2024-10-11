46ca013fadafb9ee2441a01d3b7013abaed9938e
taisooqq <cookehwk@gmail.com>
2024-10-11 17:21:03 +0900

    create new topic

--- support/kafka/src/main/kotlin/com/example/springkotlin/kafka/KafkaConfig.kt
+++ support/kafka/src/main/kotlin/com/example/springkotlin/kafka/KafkaConfig.kt
@@ -1,4 +1,14 @@
 package com.example.springkotlin.kafka
 
+import org.apache.kafka.clients.admin.NewTopic
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.TopicBuilder
+
+@Configuration
 class KafkaConfig {
+    @Bean
+    fun topic1(): NewTopic?{
+        return TopicBuilder.name("topic1").build()
+    }
 }
